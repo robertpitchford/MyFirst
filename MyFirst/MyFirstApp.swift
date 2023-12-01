9a38812cffe3d34af4ceaa38505474be928797ab
robert@pitchfords.net <5F4846B8-9F21F39D-8F5DADF2-4D0CC0A7-7D6EF7F2>
2023-12-01 11:22:43 -0800

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Robert Pitchford on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
